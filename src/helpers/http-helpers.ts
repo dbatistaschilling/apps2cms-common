c811414a0cdb70cb0f06be7767a13d64f8afb850
Davi Schilling <davi.bschilling@mobilesoft.it>
2020-09-23 15:41:24 +0200

    add created http helper

--- src/helpers/http-helpers.ts
+++ src/helpers/http-helpers.ts
@@ -1,6 +1,11 @@
-import { IHttpResponse } from "../protocols"
+import { IHttpResponse } from "../protocols";
 
 export const ok = (data: any): IHttpResponse => ({
   statusCode: 200,
+  body: data
+})
+
+export const created = (data: any): IHttpResponse => ({
+  statusCode: 201,
   body: data
 })
