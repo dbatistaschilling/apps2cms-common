263b2bf8eeadb0a689421afde8900efba4c37699
Davi Schilling <davi.bschilling@mobilesoft.it>
2020-09-23 13:36:09 +0200

    add build to save method

--- src/infra/mongodb/mongorepository/save.ts
+++ src/infra/mongodb/mongorepository/save.ts
@@ -3,7 +3,7 @@ import { ISave } from '../../../utils'
 export class SaveAdapter implements ISave {
   async save (collectionName: string, collectionParams: Object): Promise<any> {
     const CollectionModel = (await import(`../../../models/${collectionName}`)).default
-    const newCollection = new CollectionModel(collectionParams)
+    const newCollection = CollectionModel.build(collectionParams)
     return await newCollection.save()
   }
 }
