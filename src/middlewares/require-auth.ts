b70f72d77365f3dc28949daf7a5f16750da329ac
Davi Schilling <davi.bschilling@mobilesoft.it>
2020-09-28 14:11:54 +0200

    debugging requireAuth middleware

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express'
 import { NotAuthorizedError } from '../errors/not-authorized-error'
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req);
+  console.log('ENTROU NO REQUIRE-AUTH');
   if (!req.currentUser) {
     throw new NotAuthorizedError()
   }
