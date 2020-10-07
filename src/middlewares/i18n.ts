4151f06f33f5b8593dc926393ba034a217ecc459
davi schilling <davi.bschilling@mobilesoft.it>
2020-10-07 18:40:22 +0200

    i18n

--- src/middlewares/i18n.ts
+++ src/middlewares/i18n.ts
@@ -1,12 +1,3 @@
 import i18n from 'i18n'
-import { Express } from 'express'
 
-export const internalization = (app: Express) => {
-  i18n.configure({
-      locales: ['it','en'],
-      directory: __dirname + '/server/locales',
-      defaultLocale: 'it'
-  })
-
-  app.use(i18n.init)
-}
+export { i18n }
