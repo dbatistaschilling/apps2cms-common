bd3ddad5bcc21fe6cfb0a8f2bd439f2c00b1684b
davi schilling <davi.bschilling@mobilesoft.it>
2020-10-07 19:09:38 +0200

    bodyParser

--- src/middlewares/body-parser.ts
+++ src/middlewares/body-parser.ts
@@ -1,24 +1,24 @@
-// import { Request, Response } from 'express'
-// import bodyParser from 'body-parser'
+import { Request, Response } from 'express'
+import bodyParser from 'body-parser'
 
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       rawBody?: String
-//     }
-//   }
-// }
+declare global {
+  namespace Express {
+    interface Request {
+      rawBody?: String
+    }
+  }
+}
 
-// let rawBodySaver = function(req: Request, res: Response, buf: Buffer, encoding: any) {
-//   if (buf && buf.length) {
-//       req.rawBody = buf.toString(encoding || 'utf8');
-//   }
-// }
-// bodyParser.json({ verify: rawBodySaver })
-// bodyParser.urlencoded({ verify: rawBodySaver, extended: false })
-// bodyParser.raw({ verify: rawBodySaver, type: 'application/json' })
+let rawBodySaver = function(req: Request, res: Response, buf: Buffer, encoding: any) {
+  if (buf && buf.length) {
+      req.rawBody = buf.toString(encoding || 'utf8');
+  }
+}
+bodyParser.json({ verify: rawBodySaver })
+bodyParser.urlencoded({ verify: rawBodySaver, extended: false })
+bodyParser.raw({ verify: rawBodySaver, type: 'application/json' })
 
-// export { bodyParser }
+export { bodyParser }
 
-import { json } from 'express'
-export const bodyParser = json()
+// import { json } from 'express'
+// export const bodyParser = json()
