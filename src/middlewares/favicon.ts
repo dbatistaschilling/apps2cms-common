c78d4a6e9137d4f7b72ba6d1b47fd581d083f5fd
davi schilling <davi.bschilling@mobilesoft.it>
2020-10-07 18:16:04 +0200

    favicon

--- src/middlewares/favicon.ts
+++ src/middlewares/favicon.ts
@@ -1,6 +1,8 @@
+import { Request, Response, NextFunction } from 'express'
 import favicon from 'serve-favicon'
 import path from 'path'
 
-export const appIcon = () => {
+export const appIcon = (req: Request, res: Response, next: NextFunction) => {
   favicon(path.join(__dirname, 'public', 'favicon.ico'))
+  next()
 }
