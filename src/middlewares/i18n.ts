8126924746b14437f175c927e04b00727e669301
davi schilling <davi.bschilling@mobilesoft.it>
2020-10-07 17:11:04 +0200

    fix i18n

--- src/middlewares/i18n.ts
+++ src/middlewares/i18n.ts
@@ -1,7 +1,9 @@
-import internalization from 'i18n'
+import i18n from 'i18n'
 
-export const i18n = internalization.configure({
+i18n.configure({
     locales: ['it','en'],
     directory: __dirname + '/server/locales',
     defaultLocale: 'it'
 })
+
+export { i18n }
